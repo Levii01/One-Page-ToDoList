72da9a040c8b80fd0a59d58df5c94e96d0707859
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-02-27 23:45:43 +0100

    Seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,22 @@
 User.create!(email: 'admin@admin.com',password: 'adminadmin')
+user = User.first
+Task.create!(
+  name: "Hello world",
+  complete: true,
+  user_id: user.id
+)
+Task.create!(
+  name: "do new app",
+  complete: true,
+  user_id: user.id
+)
+Task.create!(
+  name: "Drink coffe",
+  complete: false,
+  user_id: user.id
+)
+Task.create!(
+  name: "Log in to get your own ToDoList:)",
+  complete: false,
+  user_id: user.id
+)
